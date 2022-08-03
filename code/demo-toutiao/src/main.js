7acb6a02e12c0e9647a5a52a55e60d3a87cecc54
茶溢 <94987272+ChaYiz@users.noreply.github.com>
2022-08-03 19:09:27 +0800

    Vue的小程序demo
    首次上传

---
+++ code/demo-toutiao/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// 导入并安装 Vant 组件库
+import Vant from 'vant'
+// 切记：为了能够覆盖默认的 less 变量，这里一定要把后缀名改为 .less
+import 'vant/lib/index.less'
+
+Vue.use(Vant)
+
+Vue.config.productionTip = false
+
+// 这样开发太麻烦了，不建议使用
+// Vue.prototype.$http = axios
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
